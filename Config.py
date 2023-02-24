dd541c7075c19bb017196217a4f42bc1ca597379
Ahmed <121744773+ahmedf0@users.noreply.github.com>
2023-02-24 13:38:24 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6213538"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6122404059:AAG7Mw9RtMSxl7TT3H3uU2Fj1K2bgz9Ywwc")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOGkBuxv5nutJBM-SkeB52igR6bGSOWdkL1fTrOlOkRGdTL064LBO6T4rLpS1RQFWQrtbNgJfh1JeW-Em1ERv0i9FE-hSlZrFcE_uKCBA1urYinwhGlRfN5zcck8QYvYTqWW7GDdmuysSMAmv8J3JfvwdRjMuWh_Sye9vj7vhnPT1NuG2BAL5hJ8nwKZQhKqgFjzWdnBIKCbYM_Ng7k_del-WF912pONbutM5xe02ve2zFdVZB03-Wv-kytb-33bN7bkclwubwUCwsdEgDojn1O0_VR3IqcxT-dloD0I-BgCPQvx5TGtY3TXWeGGtbgFkmOngvxaARgzEk4jpKdYD7lI4Sq4=")
+    API_ID = int(os.environ.get("APP_ID", "27882738"))
+    API_HASH = os.environ.get("API_HASH", "5611a2d437fadf322c3166d5cf3c28b6")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5863690899:AAFC4JYNCcHWG6Y-ljwZpczJDFZKJh0l5Kg")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "BAAjpTA1BDBIZsDH9wsvi5ofLLkxrKq290UMMbVzXeRNm3HPconfotWLJ2SUzUZSUJ2GXxNiy9_269W7uiPxzBHsf-4koza267M5oT35inuz81Rh1YgCPUePYiM_xRx8kcxnAnVSVdw-UPfQ4Mvj3hb-xZ1GfDWR-Q_qLKz8o1Pe1YQ90AhTix3lWVvBqAGwtJdKZy2cUdPfntjOxBQtjcLaAGlsO8PzuA6Tz2cg9MMK9xeR_y5uuWUSrCptMrKN3c5J3HQJVRAA6rm5VbjT86AdS_U-B_E3UXemUKoPjyi4mtosrcHQkv7v6zFmVWL2V5GvCIYhMzAqGmjjvjZPnZfaAAAAAXU2HF0A")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
